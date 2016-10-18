c13c3af6d202a539eb572d7ff3475c84ccdc24e2
Brendan Ward <bcward@consbio.org>
2016-10-17 22:27:54 -0700

    trying tap modifiers

--- keyboard/atreus/keymap_qwerty_bcw.c
+++ keyboard/atreus/keymap_qwerty_bcw.c
@@ -4,29 +4,29 @@
 /* https://github.com/tmk/tmk_core/blob/master/doc/keycode.txt */
 
 /*
- *  Q    W    E     R     T        ||       Y     U     I       O      P
- *  A    S    D     F     G        ||       H     J     K       L      enter
- *  Z    X    C     V     B        ||       N     M     ,       .      '  
- * esc  tab super shift space ctrl || alt bkspc   fn    L3      SAVE   ;
+ *  Q    W    E     R     T            ||        Y               U     I       O      P
+ *  A    S    D     F     G            ||        H               J     K       L      enter
+ *  Z(shift)    X    C     V     B     ||        N               M     ,       .      '(shift)
+ * esc  tab no shift space    ctrl     || bkspc  space   fn    L3      SAVE   ;
  */
 #define LAYER_0 KEYMAP( \
-  KC_Q,   KC_W,   KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,   KC_I,    KC_O,       KC_P, \
-  KC_A,   KC_S,   KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,   KC_K,    KC_L,       KC_ENT, \
-  KC_Z,   KC_X,   KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,   KC_COMM, KC_DOT,     KC_QUOT, \
-  KC_ESC, KC_TAB, KC_NO, KC_LSFT, KC_SPC, KC_LCTL,  KC_LGUI, KC_BSPC, KC_FN0, KC_FN3,  CTRL(KC_S),  KC_SCLN)
+  KC_Q,   KC_W,   KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,   KC_I,    KC_O,       KC_P, \
+  KC_A,   KC_S,   KC_D,    KC_F,    KC_G,                       KC_H,    KC_J,   KC_K,    KC_L,       KC_ENT, \
+  KC_FN5, KC_X,   KC_C,    KC_V,    KC_B,                       KC_N,    KC_M,   KC_COMM, KC_DOT,     KC_FN6, \
+  KC_ESC, KC_TAB, KC_BSPC, KC_LSFT, KC_SPC, KC_LCTL,    KC_FN7, KC_SPC, KC_FN0, KC_FN3,  CTRL(KC_S),  KC_SCLN)
 
 
 /*
- *  1   2   3      4      5           ||      6      7     8     9      0
- *  {   }   [      ]      -           ||      home   left  up    right  end
- *  (   )   /      \      =           ||      no     pgup  down  pgdn   `
- *  L2  no  super  shift  space  ctrl || alt  bkspc  fn    L2    no     del
+ *  1   2   3      4      5           ||       6      7     8     9      0
+ *  {   }   [      ]      -           ||       home   left  up    right  end
+ *  (   )   /      \      =           ||       no     pgup  down  pgdn   `
+ *  L2  no  no  shift   space  ctrl   || alt   bkspc  fn    L2    no     del
  */
 #define LAYER_1 KEYMAP( \
   KC_1,           KC_2,           KC_3,     KC_4,      KC_5,                     KC_6,     KC_7,     KC_8,     KC_9,      KC_0, \
-  SHIFT(KC_LBRC), SHIFT(KC_RBRC), KC_LBRC,  KC_RBRC,   KC_MINS,                   KC_HOME,  KC_LEFT,  KC_UP,    KC_RIGHT,  KC_END,  \
+  SHIFT(KC_LBRC), SHIFT(KC_RBRC), KC_LBRC,  KC_RBRC,   KC_MINS,                  KC_HOME,  KC_LEFT,  KC_UP,    KC_RIGHT,  KC_END,  \
   SHIFT(KC_9),    SHIFT(KC_0),    KC_SLASH, KC_BSLASH, KC_EQL,                   KC_NO,   KC_PGUP,  KC_DOWN,  KC_PGDN,   KC_GRAVE,  \
-  KC_FN1,         KC_NO,          KC_LGUI,  KC_LSFT,   KC_SPC, KC_LCTL, KC_LGUI, KC_BSPC,  KC_FN0,   KC_FN1,   KC_NO,    KC_DEL)
+  KC_FN1,         KC_NO,          KC_NO,    KC_LSFT,   KC_SPC, KC_LCTL, KC_LALT, KC_BSPC,  KC_FN0,   KC_FN1,   KC_NO,    KC_DEL)
 
 
 /*
@@ -39,7 +39,7 @@
   KC_F3,  KC_F4,      KC_F5,      KC_F6,      KC_F7,                     KC_F8,    KC_F9,    KC_F10,  KC_F11, KC_F12,   \
   KC_F1,  KC_F2,      KC_NO,      KC_NO,      KC_NO,                     KC_PGUP,  KC_LEFT,  KC_UP,   KC_RIGHT, KC_ENT,   \
   KC_NO,  CTRL(KC_X), CTRL(KC_C), CTRL(KC_V), KC_FN4,                    KC_PGDN,  KC_HOME,  KC_DOWN, KC_END, KC_NO,   \
-  KC_FN2, KC_TAB,     KC_NO,    KC_LSFT,    KC_SPC, KC_LCTL,  KC_LGUI, KC_BSPC,  KC_FN2,   KC_NO,   KC_NO, KC_NO)
+  KC_FN2, KC_TAB,     KC_NO,    KC_LSFT,    KC_SPC, KC_LCTL,  KC_LGUI,   KC_BSPC,  KC_FN2,   KC_NO,   KC_NO, KC_NO)
 
 
 
@@ -53,7 +53,7 @@
   KC_PSCREEN,  KC_NO,  KC_NO,    KC_VOLD,  KC_VOLU,                  KC_NO,           KC_7,  KC_8,   KC_9,                KC_NO,   \
   KC_INSERT,   KC_NO,  KC_NO,    KC_NO,    KC_NO,                    KC_NO,           KC_4,  KC_5,   KC_6,                KC_ENT,   \
   KC_NO,       KC_NO,  KC_NO,    KC_NO,    KC_NO,                    KC_0,            KC_1,  KC_2,   KC_3,                KC_DOT,   \
-  KC_NO,       KC_NO,  KC_NO,  KC_LSFT,  KC_NO, KC_LCTL,  KC_LGUI, CTRL(KC_SLASH),  KC_NO, KC_FN3, CTRL(SHIFT(KC_F10)), KC_SLASH)
+  KC_NO,       KC_NO,  KC_NO,  KC_LSFT,  KC_NO, KC_LCTL,  KC_LGUI,   CTRL(KC_SLASH),  KC_NO, KC_FN3, CTRL(SHIFT(KC_F10)), KC_SLASH)
 
 
 /* blank layer */
@@ -89,7 +89,10 @@ const uint16_t PROGMEM fn_actions[] = {
   [1] = ACTION_LAYER_ON(2, 1),  // switch to layer 2
   [2] = ACTION_LAYER_OFF(2, 1),  // switch back to layer 0
   [3] = ACTION_LAYER_MOMENTARY(3),  // to layer 3 overlay
-  [4] = ACTION_FUNCTION(BOOTLOADER)  // RESET to bootloader
+  [4] = ACTION_FUNCTION(BOOTLOADER),  // RESET to bootloader
+  [5] = ACTION_MODS_TAP_KEY(MOD_LSFT, KC_Z),  // Holding Z is SHIFT
+  [6] = ACTION_MODS_TAP_KEY(MOD_RSFT, KC_QUOT),  // Holding ' key is SHIFT
+  [7] = ACTION_MODS_TAP_KEY(MOD_LGUI, KC_BSPC)  // tap GUI key is bpsc
 };
 
 void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
